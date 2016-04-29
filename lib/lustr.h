e8f9321260b6b246ff06ab916a02e9f7456158ad
andrew cooke <andrew@acooke.org>
2016-04-29 08:39:23 -0300

    add print / append

--- lib/lustr.h
+++ lib/lustr.h
@@ -17,14 +17,14 @@ int lustr_free(lulog *log, lustr *str, int status);
 int lustr_reserve(lulog *log, lustr *str, int n);
 int lustr_clear(lulog *log, lustr *str);
 
-int lustr_print(lulog *log, lustr *str, const char *format);
-int lustr_nprint(lulog *log, lustr *str, int max_size, const char *format);
+int lustr_print(lulog *log, lustr *str, const char *text);
+int lustr_nprint(lulog *log, lustr *str, int max_size, const char *text);
 int lustr_printf(lulog *log, lustr *str, const char *format, ...);
 int lustr_nprintf(lulog *log, lustr *str, int max_size, const char *format, ...);
 int lustr_printfv(lulog *log, lustr *str, const char *format, va_list ap);
 int lustr_nprintfv(lulog *log, lustr *str, int max_size, const char *format, va_list ap);
-int lustr_append(lulog *log, lustr *str, const char *format);
-int lustr_nappend(lulog *log, lustr *str, int max_size, const char *format);
+int lustr_append(lulog *log, lustr *str, const char *text);
+int lustr_nappend(lulog *log, lustr *str, int max_size, const char *text);
 int lustr_appendf(lulog *log, lustr *str, const char *format, ...);
 int lustr_nappendf(lulog *log, lustr *str, int max_size, const char *format, ...);
 int lustr_appendfv(lulog *log, lustr *str, const char *format, va_list ap);
