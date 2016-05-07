2cacb1a97139ce85daf9e9419909ca34e9a5031b
andrew cooke <andrew@acooke.org>
2016-05-07 20:16:17 -0300

    adding simplex noise

--- lib/lusimplex.h
+++ lib/lusimplex.h
@@ -4,5 +4,6 @@
 
 double lusimplex_noise2(double xin, double yin);
 double lusimplex_noise3(double xin, double yin, double zin);
+double lusimplex_noise4(double x, double y, double z, double w);
 
 #endif
