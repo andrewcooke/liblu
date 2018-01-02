abe8d7eccca55f94fc4f9c7c038932b892d19f0c
andrew cooke <andrew@acooke.org>
2018-01-01 22:55:04 -0300

    doc greyscale

--- lib/lu/greyscale.h
+++ lib/lu/greyscale.h
@@ -5,7 +5,17 @@
 #include "strings.h"
 #include "log.h"
 
+/**
+ * @file
+ *
+ * @brief Convert 2D double arrays to grayscale text.
+ */
+/// @{
+
+/// Write quantized levels to a string.
 int lugry_str(lulog *log, int *data, size_t nx, size_t ny, const char *scale, lustr *out);
+/// Quantize double values.
 int lugry_quantize(lulog *log, double *data, int n, int levels, int **out);
 
+/// @}
 #endif
